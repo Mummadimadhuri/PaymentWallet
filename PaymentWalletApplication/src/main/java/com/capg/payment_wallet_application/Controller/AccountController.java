e21f33c1a193e7507772ad993b28b071966dc901
Bhavish Reddy <sebhavish@gmail.com>
2021-04-02 18:26:32 +0530

    Revert "DTO class and bean class committed"
    This reverts commit 9b2ccccbafc5f0ea127668a34e278394531d8808.

--- PaymentWalletApplication/src/main/java/com/capg/payment_wallet_application/Controller/AccountController.java
+++
@@ -1,7 +0,0 @@
-package com.capg.payment_wallet_application.Controller;
-
-import com.capg.payment_wallet_application.beans.BankAccount;
-import com.capg.payment_wallet_application.beans.Wallet;
-
-public class AccountController {
-}
