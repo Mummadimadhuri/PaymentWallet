9b2ccccbafc5f0ea127668a34e278394531d8808
Bhavish Reddy <sebhavish@gmail.com>
2021-04-02 17:53:28 +0530

    DTO class and bean class committed

---
+++ PaymentWalletApplication/src/main/java/com/capg/payment_wallet_application/Controller/AccountController.java
@@ -0,0 +1,7 @@
+package com.capg.payment_wallet_application.Controller;
+
+import com.capg.payment_wallet_application.beans.BankAccount;
+import com.capg.payment_wallet_application.beans.Wallet;
+
+public class AccountController {
+}
