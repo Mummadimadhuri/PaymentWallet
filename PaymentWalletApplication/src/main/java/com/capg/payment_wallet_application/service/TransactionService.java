7e29442e349d632687cc336a16fb9c660a513071
ELCOT <ELCOT@DEEPAN>
2021-04-04 20:21:10 +0530

    transaction service

--- PaymentWalletApplication/src/main/java/com/capg/payment_wallet_application/service/TransactionService.java
+++ PaymentWalletApplication/src/main/java/com/capg/payment_wallet_application/service/TransactionService.java
@@ -1,10 +1,9 @@
 package com.capg.payment_wallet_application.service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
@@ -12,22 +11,36 @@ import org.springframework.stereotype.Service;
 import com.capg.payment_wallet_application.beans.Transaction;
 import com.capg.payment_wallet_application.beans.Wallet;
 import com.capg.payment_wallet_application.dto.TransactionDTO;
+import com.capg.payment_wallet_application.exception.InsufficientBalanceException;
 import com.capg.payment_wallet_application.exception.InvalidInputException;
 import com.capg.payment_wallet_application.repo.ITransactionRepository;
 import com.capg.payment_wallet_application.util.TransactionUtils;
 
 @Service
 public class TransactionService implements ITransactionService {
-	
+
 	@Autowired
 	ITransactionRepository transactionRepo;
-	
-	final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	public TransactionDTO addTransaction(Transaction tran) {
-		Transaction transaction = transactionRepo.save(tran);
-		return TransactionUtils.convertToTransactionDto(transaction) ;
+		BigDecimal currentBalance = tran.getWallet().getBalance();
+		BigDecimal amount = (BigDecimal.valueOf(tran.getAmount()));
+		if ((amount.compareTo(currentBalance) < 0 ) && (tran.getTransactionType().equals("SEND"))) {
+			currentBalance = currentBalance.subtract(amount);
+			tran.getWallet().setBalance(currentBalance);
+			Transaction transaction = transactionRepo.save(tran);
+			return TransactionUtils.convertToTransactionDto(transaction);
+		} 
+		else if (tran.getTransactionType().equals("RECEIVE")) {
+			currentBalance = currentBalance.add(amount);
+			tran.getWallet().setBalance(currentBalance);
+			Transaction transaction = transactionRepo.save(tran);
+			return TransactionUtils.convertToTransactionDto(transaction);
+		} 
+		else {
+			throw new InsufficientBalanceException("Balance of wallet is not Sufficient to do Transaction");
+		}
 	}
 
 	@Override
@@ -41,9 +54,9 @@ public class TransactionService implements ITransactionService {
 	@Override
 	public List<TransactionDTO> viewAllTransactions(String type) {
 		List<Transaction> list = null;
-		if(transactionTypeValidation(type)) {
-			list =  transactionRepo.viewAllTransactions(type);
-		}else {
+		if (transactionTypeValidation(type)) {
+			list = transactionRepo.viewAllTransactions(type);
+		} else {
 			throw new InvalidInputException("Transaction types are only either SEND or RECEIVE");
 		}
 		List<TransactionDTO> dtoList = TransactionUtils.convertToTransactionDtoList(list);
@@ -51,16 +64,17 @@ public class TransactionService implements ITransactionService {
 	}
 
 	@Override
-	public List<TransactionDTO> viewTransactionsByDate(@DateTimeFormat(iso = ISO.DATE) LocalDate from,@DateTimeFormat(iso = ISO.DATE) LocalDate to) {
+	public List<TransactionDTO> viewTransactionsByDate(@DateTimeFormat(iso = ISO.DATE) LocalDate from,
+			@DateTimeFormat(iso = ISO.DATE) LocalDate to) {
 		List<Transaction> list = transactionRepo.viewTransactionsByDate(from, to);
 		List<TransactionDTO> dtoList = TransactionUtils.convertToTransactionDtoList(list);
 		return dtoList;
 	}
-	
+
 	private static boolean transactionTypeValidation(String type) {
 		boolean flag = true;
-		List<String> list = Arrays.asList(new String[] {"SEND","RECEIVE"});
-		if(!list.contains(type)) {
+		List<String> list = Arrays.asList(new String[] { "SEND", "RECEIVE" });
+		if (!list.contains(type)) {
 			flag = false;
 		}
 		return flag;
