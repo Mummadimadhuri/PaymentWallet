9b90dac730b6a346cfab7945a4193ad1fdc845aa
ELCOT <ELCOT@DEEPAN>
2021-04-04 20:42:28 +0530

    accountservice

--- PaymentWalletApplication/src/main/java/com/capg/payment_wallet_application/repo/IAccountRepository.java
+++ PaymentWalletApplication/src/main/java/com/capg/payment_wallet_application/repo/IAccountRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.capg.payment_wallet_application.beans.BankAccount;
 import com.capg.payment_wallet_application.beans.Wallet;
+import com.capg.payment_wallet_application.dto.WalletDTO;
 
 public interface IAccountRepository extends JpaRepository<BankAccount, Integer> {
 
@@ -15,7 +16,7 @@ public interface IAccountRepository extends JpaRepository<BankAccount, Integer> 
 //	public Wallet removeAccount(BankAccount bacc);
 	
     @Query("select ba from BankAccount ba where ba = :bacc")
-	public BankAccount findByBankAccount(@Param(value="bacc")BankAccount bacc);
+    public WalletDTO viewAccount(@Param(value="bacc")BankAccount bacc);
   
 	@Query("select bacc from BankAccount bacc where bacc = :bacc")
 	public BankAccount findByBankAccount(@Param(value="bacc") BankAccount bacc);
