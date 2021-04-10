2c668ead24f24ec803f7c42d20158f8a5d889a54
ELCOT <ELCOT@DEEPAN>
2021-04-10 10:40:40 +0530

    changes

--- PaymentWalletApplication/src/test/java/com/capg/payment_wallet_application/service/UserServiceTest.java
+++ PaymentWalletApplication/src/test/java/com/capg/payment_wallet_application/service/UserServiceTest.java
@@ -1,12 +1,15 @@
 package com.capg.payment_wallet_application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.capg.payment_wallet_application.dto.CustomerDTO;
+import com.capg.payment_wallet_application.exception.InvalidInputException;
 
 @SpringBootTest
 class UserServiceTest {
@@ -16,8 +19,17 @@ class UserServiceTest {
 
 	@Test
 	void testValidateLogin() {
-		CustomerDTO customer = userService.validateLogin("9344479110", "Rohith@2000");
+		String mobileNo ="9344479110";
+		String password = "Rohith@2000";
+		String password1 = "Rohith2000";
+		String name ="Rohith";
+		
+		CustomerDTO customer = userService.validateLogin(mobileNo,password);
+		assertEquals(name,customer.getName());
 		assertNotNull(customer);
+		
+		assertThrows(InvalidInputException.class,()->userService.validateLogin(mobileNo,password1));
+		
 	}
 
 }
