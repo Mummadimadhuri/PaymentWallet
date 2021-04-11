e8152af17bcc7c87ec38df40430f351420c56578
Bhavish Reddy <sebhavish@gmail.com>
2021-04-11 18:01:45 +0530

    changed the generation type of account no

--- PaymentWalletApplication/src/test/java/com/capg/payment_wallet_application/service/AccountServiceImplTest.java
+++ PaymentWalletApplication/src/test/java/com/capg/payment_wallet_application/service/AccountServiceImplTest.java
@@ -34,20 +34,21 @@ class AccountServiceImplTest {
 		String ifscCode1 = "SBIn0000023";
 		String bankName = "STATEBANK";
 		String bankName2 = "STATEBANK 23554 availale in india which is held by governement,Its normally very rush peak hours";
+		int accountNo = 1256;
 		double amount = 23000;
 		double amount1 = 500;
 		CustomerDTO customer = walletService.createAccount("Bhavish", "9998765432", new BigDecimal(1000), "Bhavish@2000");
 		Wallet wallet = WalletUtils.convertToWallet(customer.getWalletDto());	
-		BankAccount bankAccount = new BankAccount(ifscCode, bankName, amount, wallet);
+		BankAccount bankAccount = new BankAccount(accountNo,ifscCode, bankName, amount, wallet);
 		WalletDTO bankAccWallet = accountService.addAccount(bankAccount);
 		assertNotNull(bankAccWallet);
 		assertEquals(wallet.getWalletId(), bankAccWallet.getWalletId());
 
-		BankAccount bankAccount1 = new BankAccount(ifscCode1, bankName, amount, wallet);
+		BankAccount bankAccount1 = new BankAccount(accountNo,ifscCode1, bankName, amount, wallet);
 		assertThrows(TransactionSystemException.class, () -> accountService.addAccount(bankAccount1));
-		BankAccount bankAccount2 = new BankAccount(ifscCode, bankName2, amount, wallet);
+		BankAccount bankAccount2 = new BankAccount(accountNo,ifscCode, bankName2, amount, wallet);
 		assertThrows(TransactionSystemException.class, () -> accountService.addAccount(bankAccount2));
-		BankAccount bankAccount3 = new BankAccount(ifscCode, bankName, amount1, wallet);
+		BankAccount bankAccount3 = new BankAccount(accountNo,ifscCode, bankName, amount1, wallet);
 		assertThrows(TransactionSystemException.class, () -> accountService.addAccount(bankAccount3));
 	}
 
@@ -58,10 +59,11 @@ class AccountServiceImplTest {
 		String ifscCode1 = "SBIn00023";
 		double amount = 20000;
 		double amount1 = 500;
+		int accountNo = 1257;
 		CustomerDTO customer = walletService.createAccount("Bhavish", "9998765433", new BigDecimal(1000), "Bhavish@2000");
 		Wallet wallet = WalletUtils.convertToWallet(customer.getWalletDto());
 
-		BankAccount bankacc = new BankAccount(ifscCode, bankName, amount, wallet);
+		BankAccount bankacc = new BankAccount(accountNo,ifscCode, bankName, amount, wallet);
 		accountService.addAccount(bankacc);
 		bankacc.setAccountNo(accountService.viewAllAccounts(wallet.getWalletId()).get(0).getAccountNo());
 		WalletDTO bankAccount = accountService.removeAccount(bankacc);
@@ -69,7 +71,7 @@ class AccountServiceImplTest {
 		assertEquals(wallet.getWalletId(), bankAccount.getWalletId());
 		assertTrue(accountService.viewAllAccounts(wallet.getWalletId()).size()==0);
 
-		BankAccount bankacc3 = new BankAccount(ifscCode1, bankName, amount1, wallet);
+		BankAccount bankacc3 = new BankAccount(accountNo,ifscCode1, bankName, amount1, wallet);
 		assertThrows(InvalidInputException.class, () -> accountService.removeAccount(bankacc3));
 	}
 
@@ -79,7 +81,6 @@ class AccountServiceImplTest {
 		Wallet wallet = WalletUtils.convertToWallet(customer.getWalletDto());
 		int walletId = wallet.getWalletId();
 		int walletId1 = 1000;
-
 		List<BankAccountDTO> bankAccList = accountService.viewAllAccounts(walletId);
 		assertNotNull(bankAccList);
 		
@@ -88,12 +89,11 @@ class AccountServiceImplTest {
 
 	@Test
 	void testViewAccount() {
+		int accountNumber = 1258;
 		CustomerDTO customer = walletService.createAccount("Bhavish", "9998765435", new BigDecimal(1000), "Bhavish@2000");
 		Wallet wallet = WalletUtils.convertToWallet(customer.getWalletDto());
-		BankAccount bankAccount = new BankAccount("SBIN0001234", "State Bank", 1000.0, wallet);
+		BankAccount bankAccount = new BankAccount(accountNumber,"SBIN0001234", "State Bank", 1000.0, wallet);
 		WalletDTO walletDto = accountService.addAccount(bankAccount);
-		bankAccount.setAccountNo(accountService.viewAllAccounts(wallet.getWalletId()).get(0).getAccountNo());
-		int accountNumber = accountService.viewAllAccounts(wallet.getWalletId()).get(0).getAccountNo();
 		String ifscCode = "SBIN0001234";
 		
 		int accountNumber1 = 1000;
